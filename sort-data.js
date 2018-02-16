4aa8b5bb5435f255b064f342e49af21da6571d3e
Geoff Coutts <geoff.coutts@gmail.com>
2018-02-16 16:10:45 +0000

    Initialized file, function will take in numbers and add to list, but will not sort yet

---
+++ sort-data.js
@@ -0,0 +1,16 @@
+var list = [];
+
+function addToArray (list, number) {
+  list.push(number);
+  return list;
+}
+
+// function sortArray (num1, num2){
+//   if ()
+// }
+
+
+console.log(addToArray(list, 5));
+console.log(addToArray(list, 6));
+console.log(addToArray(list, 4));
+console.log(addToArray(list, 9));
