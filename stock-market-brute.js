0e0965ff3fac7103bda841b3677856912380817a
Geoff Coutts <geoff.coutts@gmail.com>
2018-02-18 23:43:42 +0000

    added recursive function method to stock-market-brute.js

--- stock-market-brute.js
+++ stock-market-brute.js
@@ -5,29 +5,47 @@ So that I can see what the greatest possible profit is
 */
 
 
+// Brute-force method
 
+// function maxProfit(stockPrices){
+//   var profit = 0;
+//   //loop through every price from the beginning in order to compare them
+//   for (var i = 0; i < stockPrices.length; i++){
+//     //loop through every price starting from the current price being compared in the first loop
+//     for (var j = i; j < stockPrices.length; j++){
+//       //if profit can be made, store the amount in profit
+//       if (stockPrices[j] - stockPrices[i] > profit){
+//         profit = stockPrices[j] - stockPrices[i];
+//       }
+//     }
+//   }
+//   //if profit can be made, return the amount, otherwise, return -1 signifying no profit
+//   if (profit === 0){
+//     return -1;
+//   }
+//   return profit;
+// }
 
-function maxProfit(stockPrices){
-  var profit = 0;
-  //loop through every price from the beginning in order to compare them
-  for (var i = 0; i < stockPrices.length; i++){
-    //loop through every price starting from the current price being compared in the first loop
-    for (var j = i; j < stockPrices.length; j++){
-      //if profit can be made, store the amount in profit
-      if (stockPrices[j] - stockPrices[i] > profit){
-        profit = stockPrices[j] - stockPrices[i];
+// var profit = 0;
+
+// Recursive method
+
+function maxProfit (stockPrices, profit = 0) {
+  if (stockPrices.length > 1) {
+    for (var i = 1; i < stockPrices.length; i++) {
+      if (stockPrices[i] - stockPrices[0] > profit){
+        profit = stockPrices[i] - stockPrices[0];
       }
     }
+    return maxProfit(stockPrices.slice(1), profit);
   }
-  //if profit can be made, return the amount, otherwise, return -1 signifying no profit
-  if (profit === 0){
+  if (profit > 0) {
+    return profit;
+  } else {
     return -1;
   }
-  return profit;
 }
-
-
-
 
 console.log(maxProfit([45, 24, 35, 31, 40, 38, 11])); //should return 16
 console.log(maxProfit([45, 24, 23, 22, 15, 14, 11])); //should return -1
+console.log(maxProfit([24, 54, 32, 65, 23, 23, 25])); //should return 41
